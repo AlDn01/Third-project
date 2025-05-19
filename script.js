dd3415236b748ec5ccc5b1d3b7e0f7fd0032eb22
alaabelaiche007@gmail.com <alaabelaiche007@gmail.com>
2025-05-19 03:30:39 +0100

    Got tired

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Script loaded!");
+//DICE ROLLER PROGRAM
+
+
+function rollDice(){
+
+    const numOfDice = document.getElementById("numOfDice");
+    const diceResult = document.getElementById("diceResult");
+    const diceImages = document.getElementById("diceImages");
+    let values = [];
+    let images = [];
+
+
+    for(let i = 0; i < numOfDice; i++){
+        const value = Math.floor(Math.random() * 6 ) + 1;
+        console.log(value);
+    }
+}
